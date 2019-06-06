51a1480f3599b7190b2b8d8f2ea5619e1d9db777
Lech Perczak <lech.perczak@gmail.com>
2019-06-06 21:43:49 +0200

    NandChip: reset NAND chip before reading ID
    Some NAND Flash chips require to be reset after powerup,
    before any other command could be issued. Otherwise chip won't respond
    to the command at all.

--- NandTool/NandChip.cpp
+++ NandTool/NandChip.cpp
@@ -33,6 +33,7 @@ NandChip::NandChip(FtdiNand *fn) {
 	unsigned char id[8];
 	m_fn=fn;
 	//Try to read the 5 NAND ID-bytes and create the ID-object
+	m_fn->sendCmd(NAND_CMD_RESET);
 	m_fn->sendCmd(NAND_CMD_READID);
 	m_fn->sendAddr(0, 1);
 	m_fn->readData((char *)id, 8);
